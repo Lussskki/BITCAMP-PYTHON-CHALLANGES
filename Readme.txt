5e9fac085455f04487003157ad20108d9f993a0f
Luka Guledani <monster.striker21@gmail.com>
2023-03-14 13:27:03 +0400

    #1challenge

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+i think i complete the home work and its done,
+but i cant used statment: id else elif, for challenge work.
---
+++ main.py
@@ -0,0 +1,33 @@
+"""
+Making Hello world home work and app about the employer and student 
+"""
+def helloWorld():
+    question = input("Can you write phrase 'Hello World'")
+    answer = print("Yes, thats it congrats")
+
+
+# helloWorld()
+
+
+def talking():
+   question = input("Hello, can you intruduce yourself? ")
+   answer = print("Okay, nice to meet you, hope you enjoy with use")
+
+
+# talking()
+
+
+def hello():
+    question = input("Hi, what's your name? ")
+    answer = print("Nice to meet you Luka ")
+
+
+# hello()
+
+def concatenation():
+    x = "Hello"+" World"
+    
+
+    print(x)
+
+# concatenation()
